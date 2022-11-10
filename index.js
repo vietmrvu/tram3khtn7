0ab60b74218110de2fd0f2f06baf3c66c46a4c73
vietmrvu <91784212+vietmrvu@users.noreply.github.com>
2022-11-10 20:31:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+
+// function isCollide(a, b) {
+//     var aRect = a.getBoundingClientRect();
+//     var bRect = b.getBoundingClientRect();
+
+//     return (
+//         ((aRect.top + aRect.height) < (bRect.top)) ||
+//         (aRect.top > (bRect.top + bRect.height)) ||
+//         ((aRect.left + aRect.width) < bRect.left) ||
+//         (aRect.left > (bRect.left + bRect.width))
+//     );
+// }
+
+// function mohinh1(){
+
+
+// 	var car1 = document.querySelector('.car1')
+// 	var forcar1 = document.querySelector('.forcar1')
+// 	var rect1 = car1.getBoundingClientRect();
+// 	console.log()
+// 	console.log(isCollide(car1, forcar1))
+// 	console.log(rect1.x, rect1.y)
+// 	if  (isCollide(car1, forcar1)) { 
+
+// 		var hellop = document.querySelector(".forcar1")
+// 		hellop.innerHTML = ' <img src="deadperson.gif" class="deadperson"width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180" alt="" > '
+// 	} else { 
+// 		var hellop = document.querySelector(".forcar1")
+// 		hellop.innerHTML = ' '
+// 	}
+// }
+// function mohinh2(){
+
+
+
+
+// 	var car1 = document.querySelector('.car2')
+
+// 	var rect1 = car1.getBoundingClientRect();
+
+// 	console.log(rect1.x, rect1.y)
+// 	if (rect1.x > 500) { 
+// 		var hellop = document.querySelector(".forcar2")
+// 		hellop.innerHTML = ' <img src="deadperson.gif" class="deadperson"width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180" alt="" > '
+// 	} else { 
+// 		var hellop = document.querySelector(".forcar2")
+// 		hellop.innerHTML = ' '
+// 	}
+	
+// }
+// function mohinh3(){
+
+
+// 	var car1 = document.querySelector('.car3')
+
+
+// 	var rect1 = car1.getBoundingClientRect();
+
+// 	console.log(rect1.x, rect1.y)
+// 	if (rect1.x > 500) { 
+// 		var hellop = document.querySelector(".forcar3")
+// 		hellop.innerHTML = ' <img src="deadperson.gif" class="deadperson"width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180" alt="" > '
+// 	} else { 
+// 		var hellop = document.querySelector(".forcar3")
+// 		hellop.innerHTML = ' '
+// 	}
+
+// }
