0a5ad14a8bb3d89f1fa367d7900a590b9557a6a7
vietmrvu <91784212+vietmrvu@users.noreply.github.com>
2022-11-18 21:32:00 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,68 +1,16 @@
-
-// function isCollide(a, b) {
-//     var aRect = a.getBoundingClientRect();
-//     var bRect = b.getBoundingClientRect();
-
-//     return (
-//         ((aRect.top + aRect.height) < (bRect.top)) ||
-//         (aRect.top > (bRect.top + bRect.height)) ||
-//         ((aRect.left + aRect.width) < bRect.left) ||
-//         (aRect.left > (bRect.left + bRect.width))
-//     );
-// }
-
-// function mohinh1(){
-
-
-// 	var car1 = document.querySelector('.car1')
-// 	var forcar1 = document.querySelector('.forcar1')
-// 	var rect1 = car1.getBoundingClientRect();
-// 	console.log()
-// 	console.log(isCollide(car1, forcar1))
-// 	console.log(rect1.x, rect1.y)
-// 	if  (isCollide(car1, forcar1)) { 
-
-// 		var hellop = document.querySelector(".forcar1")
-// 		hellop.innerHTML = ' <img src="deadperson.gif" class="deadperson"width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180" alt="" > '
-// 	} else { 
-// 		var hellop = document.querySelector(".forcar1")
-// 		hellop.innerHTML = ' '
-// 	}
-// }
-// function mohinh2(){
-
-
-
-
-// 	var car1 = document.querySelector('.car2')
-
-// 	var rect1 = car1.getBoundingClientRect();
-
-// 	console.log(rect1.x, rect1.y)
-// 	if (rect1.x > 500) { 
-// 		var hellop = document.querySelector(".forcar2")
-// 		hellop.innerHTML = ' <img src="deadperson.gif" class="deadperson"width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180" alt="" > '
-// 	} else { 
-// 		var hellop = document.querySelector(".forcar2")
-// 		hellop.innerHTML = ' '
-// 	}
-	
-// }
-// function mohinh3(){
-
-
-// 	var car1 = document.querySelector('.car3')
-
-
-// 	var rect1 = car1.getBoundingClientRect();
-
-// 	console.log(rect1.x, rect1.y)
-// 	if (rect1.x > 500) { 
-// 		var hellop = document.querySelector(".forcar3")
-// 		hellop.innerHTML = ' <img src="deadperson.gif" class="deadperson"width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180"alt="" > <img src="deadperson.gif" class="deadperson" width="180" alt="" > '
-// 	} else { 
-// 		var hellop = document.querySelector(".forcar3")
-// 		hellop.innerHTML = ' '
-// 	}
-
-// }
+//pie
+  var ctxP = document.getElementById("pieChart").getContext('2d');
+  var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+      labels: ["Các lỗi vi phạm khác", "Chạy quá tốc độ", "Đi không đúng làn đường, phần đường", "Vượt xe sai quy định", "Chuyển hướng không đúng quy định", "Không đúng quy định", "Sử dụng rượu bia và các chất kích thích"],
+      datasets: [{
+        data: [136.8, 32.4, 93.6, 25.2, 32.4, 25.2, 14.4],
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", '#FF5A5E', "#5AD3D1"],
+        hoverBackgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", '#FF5A5E', "#5AD3D1"]
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
